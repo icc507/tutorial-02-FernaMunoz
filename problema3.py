8c2b4efea49ac7c14ae663982f40ec283c91d7c1
FernaMunoz <142464144+FernaMunoz@users.noreply.github.com>
2024-03-22 10:36:39 -0300

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,13 +1,23 @@
-#Problema 2  / 7 ptos x4 pruebas / 28 puntos
-#Ingreso de valores en árbol TRI-nario
-#---------------------------------------------------------------------------------
-#Confeccione un programa que lea varios números y cree un árbol trinario con listas
-# esto es igual que el binario, pero los elementos que son iguales van en una lista
-# centro, de la forma [numero, [subarbol IZQ], [mismo NUM], [subarbol DER] ]
-#---------------------------------------------------------------------------------
-#Ejemplo de entrada:
-#         20 30 90 90 8 5 90
-#La salida debe ser
-#         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def arbolTrinario(numero):
+	return [numero, [], [],[]]
+
+def insertaEnArbolTrinario(arbol,numero):
+  if arbol==[]:
+    arbol+=arbolTrinario(numero)
+  elif numero < arbol[0]:
+    insertaEnArbolTrinario(arbol[1],numero)
+  elif numero == arbol[0]:
+    insertaEnArbolTrinario(arbol[2],numero)
+  else:
+    insertaEnArbolTrinario(arbol[3],numero)
+
+
+lista = input().split()
+
+lista1 = [int(x) if x.isdigit() else x for x in lista]
+
+arbol =[]
+
+for i in lista1:
+  insertaEnArbolTrinario(arbol,i)
+print(arbol)
