af04887bcb0e0dce6732e2f18bad2cbfbed17691
FernaMunoz <142464144+FernaMunoz@users.noreply.github.com>
2024-03-22 09:32:59 -0300

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,11 +1,11 @@
-#Problema 2  / 9 ptos x4 pruebas / 36 puntos
-#Concatenación de listas o tuplas
-#---------------------------------------------------------------------------------
-#Confeccione un programa que lea varios elementos y los entregue de manera inversa
-#---------------------------------------------------------------------------------
-#Ejemplo de entrada:
-#         20 90 hola jiji 77
-#La salida debe ser
-#         (77, 'jiji', 'hola', 90, 20)
-t = input()
-print(t)
+t = input().split()
+
+t = [int(x) if x.isdigit() else x for x in t]
+
+#Transformo en lista y utilizo reversed para invertirla
+t_inverso = list(reversed(t))
+
+#utilizo tuple para transformarlo en tupla
+t_tupla = tuple(t_inverso)
+
+print(t_tupla)
